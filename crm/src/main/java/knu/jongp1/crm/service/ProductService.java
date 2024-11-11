cd3ef3777942e8c7000cd40872837438e954fe90
sunha park <sunhapark1004@gmail.com>
2024-11-11 17:57:26 +0900

    RestTemplateConfig 추가
    외부 API에서 데이터 받아오기 진행 중

--- crm/src/main/java/knu/jongp1/crm/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package knu.jongp1.crm.service;
-
-import knu.jongp1.crm.entity.Product;
-
-import java.util.List;
-
-public interface ProductService {
-    List<Product> getAllProducts(); // 모든 상품을 가져오는 메소드
-}
